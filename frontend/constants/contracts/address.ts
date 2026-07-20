cc77130c965cafcb247293d98bbae77657090bbe
Khemmie-Ray <atokemmy@gmail.com>
2026-07-20 23:49:11 +0100

    claims contract FE setup

--- frontend/constants/contracts/address.ts
+++ frontend/constants/contracts/address.ts
@@ -4,6 +4,7 @@ export const CONTRACT_ADDRESSES = {
   onwardBadges: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS! as Address,
   whackStake: process.env.NEXT_PUBLIC_WHACKSTAKE_ADDRESS! as Address,
   gDollar: process.env.NEXT_PUBLIC_GDOLLAR_ADDRESS! as Address,
+  onwardClaims: process.env.NEXT_PUBLIC_CLAIMS_ADDRESS! as Address,
 } as const;
 
 export const EXPLORER_BASE = "https://celoscan.io/";
